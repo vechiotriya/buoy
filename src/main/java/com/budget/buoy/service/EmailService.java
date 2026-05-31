bbafeea18f9b79ef7e2925a68d5032c7a7ccdcaa
Riya Vechiot <vechiotriya@gmail.com>
2026-05-31 12:22:42 +0530

    update mail from address

--- src/main/java/com/budget/buoy/service/EmailService.java
+++ src/main/java/com/budget/buoy/service/EmailService.java
@@ -16,32 +16,35 @@ public class EmailService {
     @Value("${RESEND_API_KEY}")
     private String apiKey;
 
+    @Value("${RESEND_FROM_ADDRESS}")
+    private String fromAddress;
+
     public EmailService() {
         this.webClient = WebClient.builder()
                 .baseUrl("https://api.resend.com")
                 .build();
     }
 
+
     public void sendOtp(String to, String name, String otp) {
 
         String html = """
-            <h2>Password Reset</h2>
-            <p>Hi %s,</p>
-            <p>Your OTP is:</p>
-            <h1>%s</h1>
-            <p>Expires in 10 minutes.</p>
-            """.formatted(name, otp);
+                <h2>Password Reset</h2>
+                <p>Hi %s,</p>
+                <p>Your OTP is:</p>
+                <h1>%s</h1>
+                <p>Expires in 10 minutes.</p>
+                """.formatted(name, otp);
 
         webClient.post()
                 .uri("/emails")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(Map.of(
-                        "from", "onboarding@resend.dev",
-                        "to", new String[]{to},
+                        "from", fromAddress,
+                        "to", new String[] { to },
                         "subject", "Buoy - Password Reset OTP",
-                        "html", html
-                ))
+                        "html", html))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
