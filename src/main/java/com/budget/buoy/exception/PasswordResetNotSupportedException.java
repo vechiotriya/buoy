62ef11327106a2e7a73677f60fb3556326dc22f3
Riya Vechiot <vechiotriya@gmail.com>
2026-06-02 16:07:16 +0530

    feat add HttpStatus to PasswordResetNotSupportedException

--- src/main/java/com/budget/buoy/exception/PasswordResetNotSupportedException.java
+++ src/main/java/com/budget/buoy/exception/PasswordResetNotSupportedException.java
@@ -1,5 +1,9 @@
 package com.budget.buoy.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PasswordResetNotSupportedException extends RuntimeException{
     public PasswordResetNotSupportedException(String message) {
         super(message);
